b80159ee12cbd74901c93709be833f405c40f08c
Dany Rybnikov <danyr@codeaurora.org>
2014-06-23 09:16:09 +0300

    Add user activity hint for bookmarks view
    Change-Id: I0c1c38b0b76902570553630b57db8c9b2816b0e2

--- src/com/android/browser/BrowserBookmarksPage.java
+++ src/com/android/browser/BrowserBookmarksPage.java
@@ -61,6 +61,7 @@ import java.util.HashMap;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import org.codeaurora.net.NetworkServices;
 
 interface BookmarksPageCallbacks {
     // Return true if handled
@@ -434,6 +435,9 @@ public class BrowserBookmarksPage extends Fragment implements View.OnCreateConte
         // Start the loaders
         LoaderManager lm = getLoaderManager();
         lm.restartLoader(LOADER_ACCOUNTS, null, this);
+
+        //Notify about anticipated network activity
+        NetworkServices.HintUpcomingUserActivity();
 
         return mRoot;
     }
