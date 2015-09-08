a3ed2fe5946d3d757e07d6f8758d600dca3dd65c
jrizzoli <joey@cyanogenmoditalia.it>
2015-09-08 21:44:50 +0200

    Update navigation bar UI
    - Set right colors for elements
    - Hide separator when editing url
    - Keep it colored on tabs view
    - Make Security state icon smaller
    - Update progress bar indicator
    - Update incognito colors
    
    Change-Id: I856dd3add116dbc75be64a989983cac7ff814d95
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(WITH_GELLO_SOURCE),true)
-include $(LOCAL_PATH)/prebuilt/src.mk
+include $(LOCAL_PATH)/prebuilt/Android.mk
 else
 include $(LOCAL_PATH)/src/Android.mk
 endif
